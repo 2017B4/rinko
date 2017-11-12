5c8165cdfa8d5832489591eb286589c28b2e1688
funasaka <c501737055@tokushima-u.ac.jp>
2017-11-12 17:13:46 +0900

    最新版

--- identification.py
+++ identification.py
@@ -36,13 +36,11 @@ def def_sunny_param():
     states = ('雨', '晴れ') # 状態の定義
     observations = ('散歩','買い物','掃除') # ボブの行動の定義
 
-    print("状態集合：{}".format(states))
-    print("出力記号集合:{}\n".format(observations))
-    s = {'雨':0., '晴れ':1.} # 初期状態確率
+    s = {'雨':0.2, '晴れ':0.8} # 初期状態確率
 
     t = { # 各状態における状態遷移確率
-        '雨': {'雨':0., '晴れ':1.},
-        '晴れ': {'雨':0., '晴れ':1.},
+        '雨': {'雨':0.2, '晴れ':0.8},
+        '晴れ': {'雨':0.2, '晴れ':0.8},
     }
 
     e = { # 各状態における出力確率
@@ -75,13 +73,11 @@ def def_rainy_param():
     states = ('雨', '晴れ') # 状態の定義
     observations = ('散歩','買い物','掃除') # ボブの行動の定義
 
-    print("状態集合：{}".format(states))
-    print("出力記号集合:{}\n".format(observations))
-    s = {'雨':1., '晴れ':0.} # 初期状態確率
+    s = {'雨':0.8, '晴れ':0.2} # 初期状態確率
 
     t = { # 各状態における状態遷移確率
-        '雨': {'雨':1., '晴れ':0.},
-        '晴れ': {'雨':1., '晴れ':0.},
+        '雨': {'雨':0.8, '晴れ':0.2},
+        '晴れ': {'雨':0.8, '晴れ':0.2},
     }
 
     e = { # 各状態における出力確率
@@ -90,13 +86,17 @@ def def_rainy_param():
     }
     #状態、出力記号、初期状態確率、状態遷移確率、出力確率の順に値を返す
     return states,observations,s,t,e
-
+    
 if __name__ == "__main__":
     states,observations,s,t,e  =def_sunny_param()
     sunny_model = enshu_hmm.make_hmm(states,observations,s,t,e)
+    print("晴れが多い地域(sunny_model)のパラメータを表示します")
+    print("初期状態確率:{},\n状態遷移確率:{},\n出力確率:{}\n".format(s,t,e))
 
     states,observations,s,t,e = def_rainy_param()
     rainy_model = enshu_hmm.make_hmm(states,observations,s,t,e)
+    print("雨が多い地域(rainy_model)のパラメータを表示します")
+    print("初期状態確率:{},\n状態遷移確率:{},\n出力確率:{}\n".format(s,t,e))
 
     sun_X = sunny_model.sample(10)[0]
     rain_X = rainy_model.sample(10)[0]
@@ -104,51 +104,40 @@ if __name__ == "__main__":
     sun_bob = [observations[int(x)] for x in sun_X]    
     rain_bob = [observations[int(x)] for x in rain_X]    
 
-    print("ボブの行動順を以下の選択肢から選択してください(1 or 2)")
-    print("1. 雨が全く降らない地域:{}".format(sun_bob))
-    print("2. 雨しか降らない地域:{}\n".format(rain_bob))
+    print("観測系列を選択してください(0 or 1 or 2)\n")
+    print("0. プログラム終了")
+    print("1. 晴れが多い地域:{}".format(sun_bob))
+    print("2. 雨　が多い地域:{}\n".format(rain_bob))
 
     while(1):
-        which = input("どっち? >> ")
+        which = input(">> ")
         if which == '0':
             break
         elif which == "1":
-            print("あなたは晴れ易い地域のボブを選択しました。尤度を比較します。")
+            print("観測系列1.について評価。尤度を比較します。")
             sun_score = np.exp(sunny_model.score(sun_X))
             rain_score = np.exp(rainy_model.score(sun_X))
             if sun_score > rain_score:
                ans = "sunny_model"
             else :
                ans = "rainy_model"
-            print("sunny_modelの尤度sun_score:{:10f}".format(sun_score))
-            print("rainy_modelの尤度rain_score:{:10f}".format(rain_score))
-            print("sun_score > rain_score == {}".format(sun_score > rain_score))
-            print("よって,1.の観測系列を出力したモデルは{}と判定しました。".format(ans))
+            print("sunny_modelの尤度S:{:10f}".format(sun_score))
+            print("rainy_modelの尤度R:{:10f}".format(rain_score))
+            print("S > R == {}\n".format(sun_score > rain_score))
+            print("結果: 1.の観測系列を出力したモデルは{}と判定しました。".format(ans))
         elif which == "2":
-            print("あなたは雨の多い地域のボブを選択しました。尤度を比較します。")
+            print("観測系列2.について評価。尤度を比較します。")
             sun_score = np.exp(sunny_model.score(rain_X))
             rain_score = np.exp(rainy_model.score(rain_X))
             if sun_score > rain_score:
                ans = "sunny_model"
             else :
                ans = "rainy_model"
-            print("sunny_modelの尤度sun_score:{:10f}".format(sun_score))
-            print("rainy_modelの尤度rain_score:{:10f}".format(rain_score))
-            print("sun_score > rain_score == {}".format(sun_score > rain_score))
-            print("よって,2.の観測系列を出力したモデルは{}と判定しました。".format(ans))
+            print("sunny_modelの尤度S:{:10f}".format(sun_score))
+            print("rainy_modelの尤度R:{:10f}".format(rain_score))
+            print("R > S == {}\n".format(rain_score > sun_score))
+            print("結果: 2.の観測系列を出力したモデルは{}と判定しました。".format(ans))
+
         else:
             print("'1'か'2'で答えてください.終了するときは'0'を入力してください.")
     
-    
-
-
-
-
-
-
-
-
-
-
-
-
